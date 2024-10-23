2d4443969f2621593c518ba7858833c205a2062a
jetson <jetson@racecar>
2024-10-23 09:24:20 +0200

    script

--- tools/cam_get_pic.py
+++ tools/cam_get_pic.py
@@ -8,17 +8,22 @@ config = rs.config()
 config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 pipeline.start(config)
 
-#try:
-counter = 0
-#while True:
+counter = 108
+print("waiting 10s to init camera")
 sleep(10)
-for i in range(1):
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
+try:
+    while True:
+        sleep(1)
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
 
-    if color_frame:            
-        cv_image = np.asanyarray(color_frame.get_data())
-        cv2.imwrite(f"test_{i}.jpg", cv_image)
+        if color_frame:            
+            cv_image = np.asanyarray(color_frame.get_data())
+            cv2.imwrite(f"./lab_pics/test_{counter}.jpg", cv_image)
+            counter += 1
+            print(f"Successfully saved pic #{counter}!")
+except KeyboardInterrupt:
+    pass
 
 # frames = pipeline.wait_for_frames()
 # counter = 1
