23557f21c922144d45d39ec2c29a741449da6877
1@2.de <1@2.de>
2024-10-29 17:40:16 +0100

    fix install script

--- scripts/install_workstation.sh
+++ scripts/install_workstation.sh
@@ -1,5 +1,6 @@
 read -p 'Name for the conda virtual environment (press Enter for "lanedet"): ' env_name
 if [[ "$env_name" == "" ]]
+then
     env_name = 'lanedet'
 fi
 conda create -n $env_name python=3.8 -y
@@ -7,6 +8,6 @@ conda activate $env_name
 
 pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
 
-pip install -r requirements_workstation.txt
+pip install -r scripts/requirements_workstation.txt
 
 python setup.py build develop
