52f74c7d3a90d424e6d1c7589b1bb9f71aa0df7b
alkoferTim <tim.alkofer@outlook.de>
2024-12-03 13:01:16 +0900

    parse Backbone, head and Datatype

--- tools/log_parser.py
+++ tools/log_parser.py
@@ -3,16 +3,28 @@ import time
 from datetime import datetime
 import json
 import regex
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 class Log:
+    head_pattern: str = r'(?<=\(heads\):\s)(.*?)(?=\()'
+    backbone_pattern: str = r'(?<=\(backbone\):\s)(.*?)(?=\()'
+    dataset_pattern: str = r"(?<=dataset_type\s=\s')(.*?)(?=')"
+
     def __init__(self, log_lines: list):
         self.log_lines: list[LogLine] = []
+        self.head: str = None
         for line in log_lines:
             if regex.search(LogLine.timestamp_pattern, line):
-                self.log_lines.append(LogLine.from_line(line))
-    
+                self.log_lines.append(LogLine.from_line(line, self.head))
+            elif regex.search(Log.head_pattern, line):
+                self.head: str = regex.search(Log.head_pattern, line).group()
+            elif regex.search(Log.backbone_pattern, line):
+                self.backbone: str = regex.search(Log.backbone_pattern, line).group()
+            elif regex.search(Log.dataset_pattern, line): # Todo nicht alle configs gleich!
+                self.dataset: str = regex.search(Log.dataset_pattern, line).group()
+
     def plot(self):
         times = []
         steps = []
@@ -34,8 +46,8 @@ class Log:
                     continue
                 times.append(line.timestamp)
                 steps.append(line.step)
-                seg_losses.append(line.seg_loss)
-                exit_losses.append(line.exist_loss)
+                # seg_losses.append(line.seg_loss)
+                # exit_losses.append(line.exist_loss)
                 
             elif isinstance(line, JSONStatsLine):
                     metric_steps.append(steps[-1]) #applies for JSONStatsLine and BestMetricLine
@@ -50,9 +62,10 @@ class Log:
     
         #plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%HH'))
         #plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=2))
-        plt.plot(steps, seg_losses, label="seg_loss")
-        plt.plot(steps, exit_losses, label="exit_loss")
+        # plt.plot(steps, seg_losses, label="seg_loss")
+        # plt.plot(steps, exit_losses, label="exit_loss")
         plt.plot(metric_steps, best_metrics, label="best_metric")
+        #Todo: following stats in extra graph --> dimensions too different
         plt.plot(metric_steps, tps, label="tp")
         plt.plot(metric_steps, fps, label="fp")
         plt.plot(metric_steps, fns, label="fn")
@@ -60,6 +73,7 @@ class Log:
         plt.plot(metric_steps, recalls, label="recall")
         plt.plot(metric_steps, f1s, label="f1")
         plt.legend(loc="upper right")
+        #Todo: axes labels, title of graph
         plt.waitforbuttonpress()
 
 class LogLine:
@@ -69,7 +83,7 @@ class LogLine:
     msg_pattern: str = r'(?<=' + timestamp_pattern + r'\s-\s.*?\s-\s\w*\s\-\s).*'
 
     @staticmethod
-    def from_line(line: str):
+    def from_line(line: str, head: str):
         timestamp = datetime.strptime(regex.search(LogLine.timestamp_pattern, line).group(), '%Y-%m-%d %H:%M:%S,%f')
         module: str = regex.search(LogLine.module_patttern, line).group()
         level: str = regex.search(LogLine.level_pattern, line).group()
@@ -78,7 +92,7 @@ class LogLine:
         logline: LogLine = LogLine(timestamp, module, level, msg)
         if module == "lanedet.utils.recorder":
             if msg.startswith("epoch:"):
-                return EpochLine(logline)
+                return EpochLine(logline, head)
 
             elif msg.startswith("Best metric:"):
                 return BestMetricLine(logline)
@@ -99,25 +113,61 @@ class LogLine:
         self.msg: str = msg
         
 class EpochLine(LogLine):
+    #shared patterns
     epoch_pattern: str = r'(?<=epoch:\s)(\d*)'
     step_pattern: str = r'(?<=step:\s)(\d*)'
     lr_pattern: str = r'(?<=lr:\s)(\d*\.\d*)'
-    seg_loss_pattern: str = r'(?<=seg_loss:\s)(\d*\.\d*)'
-    exist_loss_pattern: str = r'(?<=exist_loss:\s)(\d*\.\d*)'
     data_pattern: str = r'(?<=data:\s)(\d*\.\d*)'
     batch_pattern: str = r'(?<=batch:\s)(\d*\.\d*)'
     eta_pattern: str = r'(?<=ETA:\s)(\d*\.\d*)'
+    #head specific patterns
+    #LaneATT
+    loss_pattern: str = r'(?<=loss:\s)(\d*\.\d*)'
+    clc_loss_pattern: str = r'(?<=clc_loss:\s)(\d*\.\d*)'
+    reg_loss_pattern: str = r'(?<=reg_loss:\s)(\d*\.\d*)'
+    #CondLaneHead
+    hm_loss_pattern: str = r'(?<=hm_loss:\s)(\d*\.\d*)'
+    kps_loss_pattern: str = r'(?<=kps_loss:\s)(\d*\.\d*)'
+    row_loss_pattern: str = r'(?<=row_loss:\s)(\d*\.\d*)'
+    range_loss_pattern: str = r'(?<=range_loss:\s)(\d*\.\d*)'
+    #LaneSeg
+    seg_loss_pattern: str = r'(?<=seg_loss:\s)(\d*\.\d*)'
+    #LaneCls
+    cls_loss_pattern: str = r'(?<=cls_loss:\s)(\d*\.\d*)'
 
-    def __init__(self, logline: LogLine):
+    #exampleLog
+    #exist_loss_pattern: str = r'(?<=exist_loss:\s)(\d*\.\d*)'
+
+
+    def __init__(self, logline: LogLine, head: str):
         super().__init__(logline.timestamp, logline.module, logline.level, logline.msg)
+        #shared properties
         self.epoch: int = int(regex.search(EpochLine.epoch_pattern, logline.msg).group())
         self.step: int = int(regex.search(EpochLine.step_pattern, logline.msg).group())
         self.lr: float = float(regex.search(EpochLine.lr_pattern, logline.msg).group())
-        self.seg_loss: float = float(regex.search(EpochLine.seg_loss_pattern, logline.msg).group())
-        self.exist_loss: float = float(regex.search(EpochLine.exist_loss_pattern, logline.msg).group())
         self.data: float = float(regex.search(EpochLine.data_pattern, logline.msg).group())
         self.batch: float = float(regex.search(EpochLine.batch_pattern, logline.msg).group())
-        #self.eta: time 
+        self.eta: time
+
+        #head specific properties
+        if head == "LaneATT":
+            self.loss: float = float(regex.search(EpochLine.loss_pattern, logline.msg).group())
+            self.cls_loss: float = float(regex.search(EpochLine.cls_loss_pattern, logline.msg).group())
+            self.reg_loss: float = float(regex.search(EpochLine.reg_loss_pattern, logline.msg).group())
+        elif head == "CondLaneHead":
+            self.loss: float = float(regex.search(EpochLine.loss_pattern, logline.msg).group())
+            self.hm_loss: float = float(regex.search(EpochLine.hm_loss_pattern, logline.msg).group())
+            self.kps_loss: float = float(regex.search(EpochLine.kps_loss_pattern, logline.msg).group())
+            self.row_loss: float = float(regex.search(EpochLine.row_loss_pattern, logline.msg).group())
+            self.range_loss: float = float(regex.search(EpochLine.range_loss_pattern, logline.msg).group())
+        elif head == "LaneSeg":
+            self.seg_loss: float = float(regex.search(EpochLine.seg_loss_pattern, logline.msg).group())
+        elif head == "LaneCls":
+            self.cls_loss: float = float(regex.search(EpochLine.cls_loss_pattern, logline.msg).group())
+        else:
+            print("Unknown head: " + head)
+        #self.exist_loss: float = float(regex.search(EpochLine.exist_loss_pattern, logline.msg).group())
+
 
 class BestMetricLine(LogLine):
     best_metric_pattern: str = r'(?<=Best metric:\s)(\d.\d*)'
@@ -143,5 +193,11 @@ def parse_file(file_path: str):
         log.plot()
 
 if __name__ == '__main__':
-    path = os.path.dirname(os.path.abspath(__file__)) + "/example.log"
+    # parser = argparse.ArgumentParser(description='Parse log file and plot metrics.')
+    # parser.add_argument('log_file', type=str, help='Path to the log file')
+    # args = parser.parse_args()
+    # path = args.log_file + "/log.txt"
+    path = ".\\work_dirs\\CULane\\20241126_032949_lr_3e-04_b_8\\" + "/log.txt"
+    #path = os.path.dirname(os.path.abspath(__file__)) + "/example.log"
+
     parse_file(path)
