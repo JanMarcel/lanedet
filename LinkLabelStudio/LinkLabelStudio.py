54e09dbe022867acd00eb15957c01d04e5757815
alkoferTim <tim.alkofer@outlook.de>
2024-12-05 09:14:03 +0900

    merged changes to lab_pics

---
+++ LinkLabelStudio/LinkLabelStudio.py
@@ -0,0 +1,70 @@
+import json
+import cv2
+global current_img
+
+def convert(path: str):
+    project: dict = readJSON(path)
+    for picture in project:
+        convert_pic(picture)
+
+def readJSON(path: str) -> dict:
+    with open(path) as f:
+        return json.load(f)
+
+def convert_pic(picture: dict):
+    global current_img
+    print(f'convert picture with id {picture["id"]} and name {picture["file_upload"]}')   
+    current_img = cv2.imread("LinkLabelStudio/" + picture["file_upload"]) # Todo File path dynamic
+    for annotation in picture["annotations"]: 
+        convert_annotation(annotation)
+    cv2.imshow('view', current_img)
+    cv2.waitKey(0)
+
+def convert_annotation(annotation: dict):
+    print(f'\t convert annotation with id {annotation["id"]}')
+    h_samples: list[int] = []
+    for result in annotation["result"]:
+        h_samples.append(result["value"]["y"])
+        #convert_annotation_result(result)
+    h_samples.sort()
+    left: list[int] = [-2] * len(h_samples)
+    right: list[int] = [-2] * len(h_samples)
+    for result in annotation["result"]:
+        label = result["value"]["keypointlabels"]
+        if label == ["lane-left"]:
+            left[h_samples.index(result["value"]["y"])] = round(result["value"]["x"]*result["original_width"]/100)
+        elif label == ["lane-right"]:
+            right[h_samples.index(result["value"]["y"])] = round(result["value"]["x"]*result["original_width"]/100)
+        else:
+            print(f"Could not parse label: {label}")
+    
+    #create line for target_file
+    dic = {}
+    dic["lanes"] = [left, right]
+    dic["h_samples"] = h_samples
+    for i in range(len(h_samples)):
+        dic["h_samples"][i] = round(dic["h_samples"][i]*annotation["result"][0]["original_height"]/100) #Todo check for doubles
+    dic["raw_file"] = "d3b4989f-test_0.jpg" #+ annotation["file_upload"] #Todo think about directory strucure
+
+    print(dic)
+    with open("LinkLabelStudio/target_file.json", "w") as f:# Todo file-name dynamic
+        json.dump(dic, f)
+
+def convert_annotation_result(result: dict):
+    global current_img
+    print(f'\t\t convert result with id {result["id"]}')
+    label = result["value"]["keypointlabels"]
+
+    if label == ["lane-left"]:
+        print("left")
+    elif label == ["lane-right"]:
+        print("right")
+    else:
+        print(f"Could not parse label: {label}")
+    
+    point = (round(result["value"]["x"]/100 * result["original_width"]), round(result["value"]["y"]/100 * result["original_height"]))
+    print(point)
+    current_img = cv2.circle(current_img, point, radius=5, color=(0, 0, 255), thickness=-1)    
+
+    
+convert('LinkLabelStudio/project-5-at-2024-11-20-07-55-53287f8c.json')
--- tools/log_parser.py
+++ tools/log_parser.py
@@ -1,38 +1,98 @@
 import os
 import time
 from datetime import datetime
+import json
 import regex
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 class Log:
+    head_pattern: str = r'(?<=\(heads\):\s)(.*?)(?=\()'
+    backbone_pattern: str = r'(?<=\(backbone\):\s)(.*?)(?=\()'
+    dataset_pattern: str = r"(?<=dataset_type\s=\s')(.*?)(?=')"
+
     def __init__(self, log_lines: list):
         self.log_lines: list[LogLine] = []
+        self.head: str = None
         for line in log_lines:
             if regex.search(LogLine.timestamp_pattern, line):
-                self.log_lines.append(LogLine.from_line(line))
-    
-    def plot(self):
-        x = []
-        y = []
-        y1 = []
+                self.log_lines.append(LogLine.from_line(line, self.head))
+            elif regex.search(Log.head_pattern, line):
+                self.head: str = regex.search(Log.head_pattern, line).group()
+            elif regex.search(Log.backbone_pattern, line):
+                self.backbone: str = regex.search(Log.backbone_pattern, line).group()
+            elif regex.search(Log.dataset_pattern, line): # Todo nicht alle configs gleich!
+                self.dataset: str = regex.search(Log.dataset_pattern, line).group()
 
-        first: bool = True
+    def plot(self):
+        #Todo: 
+        # 2 graphs one for numbers between 0 and 1, one for > 1
+        # depending on head, different metrics
+
+        __laneatt = ["loss", "cls_loss", "reg_loss"]
+
+        if self.head == "LaneATT":
+            step_keys = __laneatt
+        #Todo: other heads
+        
+        epoch_keys_small = ["best_metric", "precision", "recall", "f1"]
+        epoch_keys_big = ["TP", "FP", "FN"]
+
+        plot_dict = {}
+        plot_dict["epochs"] = [0]
+        plot_dict["steps"] = []
+        plot_dict["step_values"] = {}
+        plot_dict["epoch_values_small"] = {}
+        plot_dict["epoch_values_big"] = {}
+
+        for k in step_keys:
+            plot_dict["step_values"][k] = []
+        
+        for k in epoch_keys_small:
+            plot_dict["epoch_values_small"][k] = []
+
+        for k in epoch_keys_big:
+            plot_dict["epoch_values_big"][k] = []
+
         for line in self.log_lines:
             if isinstance(line, EpochLine):
-                if first:
-                    first = False
-                    continue
-                x.append(line.timestamp)
-                y.append(line.seg_loss)
-                y1.append(line.exist_loss)
-    
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%HH'))
-        plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=2))
-        plt.plot(x, y)
-        plt.plot(x, y1)
-        plt.waitforbuttonpress()
+                if plot_dict["epochs"][-1] != line.epoch:
+                    plot_dict["epochs"].append(line.epoch)
+                plot_dict["steps"].append(line.step)
+                for key in step_keys:
+                    plot_dict["step_values"][key].append(getattr(line, key))
+            if isinstance(line, BestMetricLine):
+                plot_dict["epoch_values_small"]["best_metric"].append(line.best_metric)
+            if isinstance(line, JSONStatsLine):
+                for key in epoch_keys_small:
+                    if key != "best_metric":
+                        plot_dict["epoch_values_small"][key].append(getattr(line, key))
+                for key in epoch_keys_big:
+                        plot_dict["epoch_values_big"][key].append(getattr(line, key))
+                
 
+        f1 = plt.figure("1")
+        for key in step_keys:
+            plt.plot(plot_dict["steps"], plot_dict["step_values"][key], label=key)
+            plt.legend()
+        ax = plt.gca()
+        #ax.set_xlim([xmin, xmax])
+        ax.set_ylim([0, 10])
+        f2 = plt.figure("2")
+        for key in plot_dict["epoch_values_small"]:
+            plt.plot(plot_dict["epochs"], plot_dict["epoch_values_small"][key], label=key)
+            plt.legend()
+
+        f3 = plt.figure("3")
+        for key in plot_dict["epoch_values_big"]:
+            plt.plot(plot_dict["epochs"], plot_dict["epoch_values_big"][key], label=key)
+            plt.legend()
+        
+        f1.show()
+        f2.show()
+        f3.show()
+        plt.waitforbuttonpress()              
 class LogLine:
     timestamp_pattern: str = r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3}'
     module_patttern: str = r'(?<=' + timestamp_pattern + r'\s-\s)(.*?)(?=\s-\s\w*\s\-\s.*)'
@@ -40,17 +100,28 @@ class LogLine:
     msg_pattern: str = r'(?<=' + timestamp_pattern + r'\s-\s.*?\s-\s\w*\s\-\s).*'
 
     @staticmethod
-    def from_line(line: str):
+    def from_line(line: str, head: str):
         timestamp = datetime.strptime(regex.search(LogLine.timestamp_pattern, line).group(), '%Y-%m-%d %H:%M:%S,%f')
         module: str = regex.search(LogLine.module_patttern, line).group()
         level: str = regex.search(LogLine.level_pattern, line).group()
         msg: str = regex.search(LogLine.msg_pattern, line).group()
 
         logline: LogLine = LogLine(timestamp, module, level, msg)
-        if msg.startswith("epoch:"):
-            return EpochLine(logline)
-        else:
-            return logline
+        if module == "lanedet.utils.recorder":
+            if msg.startswith("epoch:"):
+                return EpochLine(logline, head)
+
+            elif msg.startswith("Best metric:"):
+                return BestMetricLine(logline)
+        elif module == "lanedet.datasets.base_dataset":
+            try:
+                stats: dict = json.loads(msg)
+                return JSONStatsLine(logline, stats)
+            except ValueError as e:
+                print(e)
+        
+        # fallback
+        return logline
 
     def __init__(self, timestamp: datetime, module: str, level: str, msg: str):       
         self.timestamp = timestamp
@@ -59,29 +130,78 @@ class LogLine:
         self.msg: str = msg
         
 class EpochLine(LogLine):
+    #shared patterns
     epoch_pattern: str = r'(?<=epoch:\s)(\d*)'
     step_pattern: str = r'(?<=step:\s)(\d*)'
     lr_pattern: str = r'(?<=lr:\s)(\d*\.\d*)'
-    seg_loss_pattern: str = r'(?<=seg_loss:\s)(\d*\.\d*)'
-    exist_loss_pattern: str = r'(?<=exist_loss:\s)(\d*\.\d*)'
     data_pattern: str = r'(?<=data:\s)(\d*\.\d*)'
     batch_pattern: str = r'(?<=batch:\s)(\d*\.\d*)'
     eta_pattern: str = r'(?<=ETA:\s)(\d*\.\d*)'
+    #head specific patterns
+    #LaneATT
+    loss_pattern: str = r'(?<=loss:\s)(\d*\.\d*)'
+    clc_loss_pattern: str = r'(?<=clc_loss:\s)(\d*\.\d*)'
+    reg_loss_pattern: str = r'(?<=reg_loss:\s)(\d*\.\d*)'
+    #CondLaneHead
+    hm_loss_pattern: str = r'(?<=hm_loss:\s)(\d*\.\d*)'
+    kps_loss_pattern: str = r'(?<=kps_loss:\s)(\d*\.\d*)'
+    row_loss_pattern: str = r'(?<=row_loss:\s)(\d*\.\d*)'
+    range_loss_pattern: str = r'(?<=range_loss:\s)(\d*\.\d*)'
+    #LaneSeg
+    seg_loss_pattern: str = r'(?<=seg_loss:\s)(\d*\.\d*)'
+    #LaneCls
+    cls_loss_pattern: str = r'(?<=cls_loss:\s)(\d*\.\d*)'
 
-    def __init__(self, logline: LogLine):
+    #exampleLog
+    #exist_loss_pattern: str = r'(?<=exist_loss:\s)(\d*\.\d*)'
+
+
+    def __init__(self, logline: LogLine, head: str):
         super().__init__(logline.timestamp, logline.module, logline.level, logline.msg)
+        #shared properties
         self.epoch: int = int(regex.search(EpochLine.epoch_pattern, logline.msg).group())
         self.step: int = int(regex.search(EpochLine.step_pattern, logline.msg).group())
         self.lr: float = float(regex.search(EpochLine.lr_pattern, logline.msg).group())
-        self.seg_loss: float = float(regex.search(EpochLine.seg_loss_pattern, logline.msg).group())
-        self.exist_loss: float = float(regex.search(EpochLine.exist_loss_pattern, logline.msg).group())
         self.data: float = float(regex.search(EpochLine.data_pattern, logline.msg).group())
         self.batch: float = float(regex.search(EpochLine.batch_pattern, logline.msg).group())
-        #self.eta: time 
-        print(self)
+        self.eta: time
+
+        #head specific properties
+        if head == "LaneATT":
+            self.loss: float = float(regex.search(EpochLine.loss_pattern, logline.msg).group())
+            self.cls_loss: float = float(regex.search(EpochLine.cls_loss_pattern, logline.msg).group())
+            self.reg_loss: float = float(regex.search(EpochLine.reg_loss_pattern, logline.msg).group())
+        elif head == "CondLaneHead":
+            self.loss: float = float(regex.search(EpochLine.loss_pattern, logline.msg).group())
+            self.hm_loss: float = float(regex.search(EpochLine.hm_loss_pattern, logline.msg).group())
+            self.kps_loss: float = float(regex.search(EpochLine.kps_loss_pattern, logline.msg).group())
+            self.row_loss: float = float(regex.search(EpochLine.row_loss_pattern, logline.msg).group())
+            self.range_loss: float = float(regex.search(EpochLine.range_loss_pattern, logline.msg).group())
+        elif head == "LaneSeg":
+            self.seg_loss: float = float(regex.search(EpochLine.seg_loss_pattern, logline.msg).group())
+        elif head == "LaneCls":
+            self.cls_loss: float = float(regex.search(EpochLine.cls_loss_pattern, logline.msg).group())
+        else:
+            print("Unknown head: " + head)
+        #self.exist_loss: float = float(regex.search(EpochLine.exist_loss_pattern, logline.msg).group())
 
 
-    
+class BestMetricLine(LogLine):
+    best_metric_pattern: str = r'(?<=Best metric:\s)(\d.\d*)'
+
+    def __init__(self, logline: LogLine):
+        super().__init__(logline.timestamp, logline.module, logline.level, logline.msg)
+        self.best_metric: float = float(regex.search(BestMetricLine.best_metric_pattern, logline.msg).group())
+
+class JSONStatsLine(LogLine):
+    def __init__(self, logline: LogLine, stats: dict):
+        super().__init__(logline.timestamp, logline.module, logline.level, logline.msg)
+        self.TP = stats['TP']
+        self.FP = stats['FP']
+        self.FN = stats['FN']
+        self.precision = stats['Precision']
+        self.recall = stats['Recall']
+        self.f1 = stats['F1']
 
 def parse_file(file_path: str):
     with open(file_path, 'r') as file:
@@ -90,5 +210,11 @@ def parse_file(file_path: str):
         log.plot()
 
 if __name__ == '__main__':
-    path = os.path.dirname(os.path.abspath(__file__)) + "/example.log"
+    # parser = argparse.ArgumentParser(description='Parse log file and plot metrics.')
+    # parser.add_argument('log_file', type=str, help='Path to the log file')
+    # args = parser.parse_args()
+    # path = args.log_file + "/log.txt"
+    path = ".\\work_dirs\\CULane\\20241126_032949_lr_3e-04_b_8\\" + "/log.txt"
+    #path = os.path.dirname(os.path.abspath(__file__)) + "/example.log"
+
     parse_file(path)
