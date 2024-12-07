e3a02cd9ff59f383715098e12991c271e3d48b7a
Jetson Orin Nano <ja122sch@htwg-konstanz.de>
2024-12-07 02:06:37 +0100

    update install Jeston

--- scripts/install_Jetson.sh
+++ scripts/install_Jetson.sh
@@ -7,11 +7,15 @@
 #conda create -n $env_name python=3.8 -y
 #conda activate $env_name
 
+pip3 install setuptools==58.3.0
+pip3 install cycler==0.10  python-dateutil==2.7.3 scipy==1.10.1 pytz==2017.3 numpy==1.24.4 #attrs==18.1.0
+pip3 install opencv-python==4.5.5.64 opencv-python-headless==4.5.5.64  kiwisolver==1.4.7 pyparsing==3.2.0 imageio==2.36.0 networkx==3.4.2 tifffile==2024.9.20 joblib==1.4.2 threadpoolctl==3.5.0 mmcv==1.7.2 mmcv-full==1.7.2
+pip3 install attrs
+pip3 install six
 pip3 install future
 pip3 install -U wheel mock pillow
 pip3 install testresources
 pip3 install pyyaml
-pip3 install setuptools==58.3.0
 pip3 install Cython
 
 pip3 install gdown
@@ -38,7 +42,11 @@ pip3 install torchvision-0.18.0a0+6043bc2-cp310-cp310-linux_aarch64.whl
 rm torchvision-0.18.0a0+6043bc2-cp310-cp310-linux_aarch64.whl
 ## NEW JETSON ORIN NANO DEV KIT INSTALLATION END
 
-pip3 install pathos addict yapf mmcv mmcv-full imgaug albumentations scikit-learn p_tqdm
-pip3 install cycler==0.10 kiwisolver==1.0.1 pyparsing==2.2.1 python-dateutil==2.7.3 opencv-python-headless==4.8.0.74 scipy==1.10.1 imageio==2.3.0 six networkx==2.0 tifffile==2019.7.26 joblib==0.11 threadpoolctl==2.0.0 pytz==2017.3 numpy==1.24.4 attrs==18.1.0
+pip3 install pathos addict yapf mmcv mmcv-full imgaug albumentations scikit-learn p_tqdm catkin_pkg #six
+
+# experimental start
+#pip3 install opencv-python-headless==4.8.0.74 kiwisolver==1.0.1 pyparsing==2.2.1 imageio==2.3.0 networkx==2.0 tifffile==2019.7.26 joblib==0.11 threadpoolctl==2.0.0
+
+# experimental end
 
 python setup.py build develop
