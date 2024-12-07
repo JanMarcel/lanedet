c4cd60b17c89ffa7df6574b23d4f9fcc680de162
Jetson Orin Nano <ja122sch@htwg-konstanz.de>
2024-12-07 02:05:40 +0100

    move detect to lanedet package for htwg_racecar, add preprocess_live

--- tools/detect.py
+++ tools/detect.py
@@ -1,81 +1,19 @@
-import numpy as np
-import torch
-import cv2
-import os
-import os.path as osp
-import glob
-import argparse
-from lanedet.datasets.process import Process
-from lanedet.models.registry import build_net
-from lanedet.utils.config import Config
-from lanedet.utils.visualization import imshow_lanes
-from lanedet.utils.net_utils import load_network
-from pathlib import Path
-from tqdm import tqdm
-#configs/laneatt/mobilenetv2_tusimple_200epochs.py --savedir vis/bla --load_from models/mobilenet_tusimple_200epochs.pth --img data/Labor/racecar_image_1.jpg
-class Detect(object):
-    def __init__(self, cfg):
-        self.cfg = cfg
-        self.processes = Process(cfg.val_process, cfg)
-        self.net = build_net(self.cfg)
-        self.net = torch.nn.parallel.DataParallel(
-                self.net, device_ids = range(1)).cuda()
-        self.net.eval()
-        load_network(self.net, self.cfg.load_from)
+# import numpy as np
+# import torch
+# import cv2
+# import os
+# import os.path as osp
+# import glob
+# import argparse
+# from lanedet.datasets.process import Process
+# from lanedet.models.registry import build_net
+# from lanedet.utils.config import Config
+# from lanedet.utils.visualization import imshow_lanes
+# from lanedet.utils.net_utils import load_network
+# from pathlib import Path
+# from tqdm import tqdm
 
-    def preprocess(self, img_path):
-        ori_img = cv2.imread(img_path)
-        img = ori_img[self.cfg.cut_height:, :, :].astype(np.float32)
-        data = {'img': img, 'lanes': []}
-        data = self.processes(data)
-        data['img'] = data['img'].unsqueeze(0)
-        data.update({'img_path':img_path, 'ori_img':ori_img})
-        return data
-
-    def inference(self, data):
-        with torch.no_grad():
-            print(data)
-            data = self.net(data)
-            print(data)
-            data = self.net.module.get_lanes(data)
-            print(data)
-        return data
-
-    def show(self, data):
-        out_file = self.cfg.savedir 
-        if out_file:
-            out_file = osp.join(out_file, osp.basename(data['img_path']))
-        lanes = [lane.to_array(self.cfg) for lane in data['lanes']]
-        imshow_lanes(data['ori_img'], lanes, show=self.cfg.show, out_file=out_file)
-
-    def run(self, data):
-        data = self.preprocess(data)
-        data['lanes'] = self.inference(data)[0]
-        if self.cfg.show or self.cfg.savedir:
-            self.show(data)
-        return data
-
-def get_img_paths(path):
-    p = str(Path(path).absolute())  # os-agnostic absolute path
-    if '*' in p:
-        paths = sorted(glob.glob(p, recursive=True))  # glob
-    elif os.path.isdir(p):
-        paths = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
-    elif os.path.isfile(p):
-        paths = [p]  # files
-    else:
-        raise Exception(f'ERROR: {p} does not exist')
-    return paths 
-
-def process(args):
-    cfg = Config.fromfile(args.config)
-    cfg.show = args.show
-    cfg.savedir = args.savedir
-    cfg.load_from = args.load_from
-    detect = Detect(cfg)
-    paths = get_img_paths(args.img)
-    for p in tqdm(paths):
-        detect.run(p)
+from lanedet.detect import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
